f6c95daeb3e38e1eaaa3ccbc6ee64cbf6d51dd19
RedT0wer <147622032+RedT0wer@users.noreply.github.com>
2026-02-15 15:04:20 +0400

    added methods

--- src/main/java/com/pancreatitis/pankreat2/modules/questionnairemanager/QuestionnaireManagerModule.java
+++ src/main/java/com/pancreatitis/pankreat2/modules/questionnairemanager/QuestionnaireManagerModule.java
@@ -1,17 +1,24 @@
 package com.pancreatitis.pankreat2.modules.questionnairemanager;
 
+import com.pancreatitis.pankreat2.models.*;
 import com.pancreatitis.pankreat2.modules.cloudstorage.CloudStorageModule;
 import com.pancreatitis.pankreat2.modules.database.DatabaseModule;
-import com.pancreatitis.pankreat2.modules.trainset.TrainSetModule;
+import javafx.util.Pair;
+
+import javax.crypto.SecretKey;
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuestionnaireManagerModule {
     private static QuestionnaireManagerModule instance;
     private DatabaseModule databaseModule;
     private CloudStorageModule cloudStorageModule;
+
     private void QuestionnaireManagerModule() {
         databaseModule = DatabaseModule.getInstance();
         cloudStorageModule = CloudStorageModule.getInstance();
     }
+
     public static QuestionnaireManagerModule getInstance() {
         if (instance == null) {
             synchronized (QuestionnaireManagerModule.class) {
@@ -21,5 +28,48 @@ public class QuestionnaireManagerModule {
             }
         }
         return instance;
+    }
+
+    public boolean saveQuestionnaire(Questionnaire questionnaire, Patient patient) {
+        try {
+            databaseModule.beginTransaction();
+
+            long idPatient = questionnaire.getIdPatient();
+            if (idPatient == -1){
+                idPatient = databaseModule.insertPatient(patient);
+                questionnaire.setIdPatient(idPatient);
+                patient.setId(idPatient);
+            }
+
+            long idQuestionnaire = questionnaire.getId();
+            if (idQuestionnaire == -1){
+                idQuestionnaire = databaseModule.insertQuestionnaire(questionnaire);
+                questionnaire.setId(idQuestionnaire);
+            } else {
+                databaseModule.updateQuestionnaire(questionnaire);
+            }
+
+            databaseModule.setTransactionSuccessful();
+            return true;
+        } catch (Exception e) {
+            return false;
+        } finally {
+            databaseModule.endTransaction();
+        }
+    }
+
+    public boolean saveValueCharacteristic(CharacterizationAnketPatient characterizationAnketPatient) {
+        try {
+            databaseModule.beginTransaction();
+
+            databaseModule.insertCharacterizationAnketPatient(characterizationAnketPatient);
+
+            databaseModule.setTransactionSuccessful();
+            return true;
+        } catch (Exception e) {
+            return false;
+        } finally {
+            databaseModule.endTransaction();
+        }
     }
 }
